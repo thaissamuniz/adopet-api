c39df82f6875a6d432ecc1b2e88f3a8f1cd50472
thaissa <thaiissa.muniiz@gmail.com>
2023-07-14 16:06:44 -0300

    add create user route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -14,6 +14,22 @@ class UserController {
             res.status(500).send(`${error.message} - deu erro`);
         }
     }
+
+    static async createUser(req, res) {
+        try {
+            const newUser = req.body;
+            if (newUser.nome && newUser.email && newUser.senha) {
+                const users = JSON.parse(fs.readFileSync("db.json"));
+                const usersUpdated = [...users, newUser]
+                fs.writeFileSync("db.json", JSON.stringify(usersUpdated));
+                res.status(201).json(newUser)
+            } else {
+                res.status(422).send('os campos nome, email e senha são obrigatórios.');
+            }
+        } catch (error) {
+            res.status(500).send(`${error.message} - erro no post`);
+        }
+    }
 }
 
 module.exports = UserController;
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
-router.get('/users', UserController.getUsers);
+router
+    .get('/users', UserController.getUsers)
+    .post('/users', UserController.createUser)
 
 
 
