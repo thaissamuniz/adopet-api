bff838f83596d19d04363fedf61a266746719ab6
thaissa <thaiissa.muniiz@gmail.com>
2023-07-21 18:10:14 -0300

    refactoring with mongoose models

--- src/controllers/ShelterController.js
+++ src/controllers/ShelterController.js
@@ -1,33 +1,30 @@
 const fs = require('fs');
+const shelters = require('../models/Shelter');
 
 
 class ShelterController {
     static async getShelters(req, res) {
         try {
-            const shelters = JSON.parse(fs.readFileSync("shelter.json"));
-            if (shelters.length < 1) {
-                res.send('nenhum abrigo cadastrado')
-            } else {
-                res.send(shelters)
+            const result = await shelters.find()
+
+            if (result !== null) {
+                res.status(200).json(result)
             }
+
         } catch (error) {
             res.status(500).send(`${error.message} - erro ao recuperar os dados`);
         }
     }
 
     static async getShelterById(req, res) {
-        const { id } = req.params;
         try {
-            const shelters = JSON.parse(fs.readFileSync("shelter.json"));
-            const shelterByIdList = shelters.filter(shelter => shelter.id === Number(id));
+            const { id } = req.params;
 
-            if (shelterByIdList.length < 1) {
-                res.send('id não existente')
-            } else if (id && Number(id)) {
-                const shelterById = shelterByIdList[0];
-                res.send(shelterById);
+            const shelter = await shelters.findById(id).exec();
+            if (shelter !== null) {
+                res.status(200).send(shelter);
             } else {
-                res.status(422).send('id inválido')
+                res.status(422).send('id não encontrado');
             }
         } catch (error) {
             res.status(500).send(`${error.message} - erro ao procurar abrigo por id`);
@@ -35,32 +32,25 @@ class ShelterController {
     }
 
     static async createShelter(req, res) {
-        const newShelter = req.body;
         try {
-            if (newShelter.nome && newShelter.telefone && newShelter.cidade && newShelter.estado && newShelter.id) {
-                const shelters = JSON.parse(fs.readFileSync("shelter.json"));
-                const sheltersUpdated = [...shelters, newShelter]
-                fs.writeFileSync("shelter.json", JSON.stringify(sheltersUpdated));
-                res.status(201).json(newShelter)
-            } else {
-                res.status(422).send('os campos id, nome, telefone, cidade e estado são obrigatórios.');
-            }
+            let shelter = new shelters(req.body);
+            const shelterResult = await shelter.save();
+            res.status(201).send(shelterResult.toJSON());
         } catch (error) {
             res.status(500).send(`${error.message} - erro no post`);
         }
     }
 
     static async updateShelter(req, res) {
-        const { id } = req.params;
-        const updatedData = req.body;
-
         try {
-            let shelters = JSON.parse(fs.readFileSync("shelter.json"));
-            const targetIdIndex = shelters.findIndex(shelter => shelter.id === Number(id));
-            const updated = { ...shelters[targetIdIndex], ...updatedData };
-            shelters[targetIdIndex] = updated;
-            fs.writeFileSync("shelter.json", JSON.stringify(shelters));
-            res.status(200).json(updated)
+            const { id } = req.params;
+
+            let shelter = await shelters.findByIdAndUpdate(id, { $set: req.body });
+            if (shelter !== null) {
+                res.status(200).send({ message: 'abrigo atualizado co sucesso' })
+            } else {
+                res.status(422).send('abrigo não localizado')
+            }
         } catch (error) {
             res.status(500).send(`${error.message} - erro ao atualizar abrigo`);
         }
@@ -70,10 +60,12 @@ class ShelterController {
         const { id } = req.params;
 
         try {
-            let shelters = JSON.parse(fs.readFileSync("shelter.json"));
-            const updatedData = shelters.filter(shelter => shelter.id !== Number(id));
-            fs.writeFileSync("shelter.json", JSON.stringify(updatedData));
-            res.send('abrigo deletado com sucesso')
+            const shelter = await shelters.findByIdAndDelete(id);
+            if (shelter !== null) {
+                res.status(200).send({ message: 'abrigo apagado com sucesso.' });
+            } else {
+                res.send('abrigo não localizado')
+            }
         } catch (error) {
             res.status(500).send(`${error.message} - erro ao deletar abrigo`);
         }
